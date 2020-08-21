201db3e68d48293437fadc7a48fb413ac6b9f970
hohoon song <thdghgns@gmail.com>
2020-08-21 22:29:05 +0900

    린트에 맞게 수정

--- vue/src/services/auth-header.js
+++ vue/src/services/auth-header.js
@@ -1,9 +1,9 @@
-export default function authHeader() {
-  let user = JSON.parse(localStorage.getItem('user'));
+export default function authHeader () {
+  const user = JSON.parse(localStorage.getItem('user'))
 
   if (user && user.accessToken) {
-    return { 'X-AUTH-TOKEN': user.accessToken };
+    return { 'X-AUTH-TOKEN': user.accessToken }
   } else {
-    return {};
+    return {}
   }
 }
