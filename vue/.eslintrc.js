89a2016816a763945a6a3c51f451d2f5d0766152
hohoon song <thdghgns@gmail.com>
2020-08-21 22:28:36 +0900

    린트 수정

--- vue/.eslintrc.js
+++ vue/.eslintrc.js
@@ -7,6 +7,17 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'vue/max-attributes-per-line': ['error', {
+      'singleline': 4,
+      'multiline': {
+        'max': 4,
+        'allowFirstLine': true
+      }
+    }],
+    'vue/html-closing-bracket-newline': ['error', {
+      'singleline': 'never',
+      'multiline': 'never'
+    }],
   },
   parserOptions: {
     parser: 'babel-eslint',
